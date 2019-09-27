a43d4103c08f80fb37d2a2e6bbe27e3c8b83d1db
Mr Wonderful <mrwonderful@Mrs-MacBook-Pro.local>
2019-09-27 11:24:02 -0400

    Adds collectionView with a search bar to be able to populate countries information based on the users input

---
+++ iOS-Collection-View-Lab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-Collection-View-Lab
+//
+//  Created by Mr Wonderful on 9/26/19.
+//  Copyright © 2019 Mr Wonderful. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
